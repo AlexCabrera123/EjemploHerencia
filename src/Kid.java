5a5e5614adb4b24baae6f20a8223984d626263a3
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-03-24 11:16:35 -0600

    Clase 24/03/22

--- src/Kid.java
+++ src/Kid.java
@@ -1,2 +1,27 @@
-public class Kid {
+public class Kid extends Personaje{
+    //colorPuerta
+    private String colorPuerta;
+
+    public String getColorPuerta() {
+        return colorPuerta;
+    }
+
+    public void setColorPuerta(String colorPuerta) {
+        this.colorPuerta = colorPuerta;
+    }
+
+    public Kid(String nombre, double estatura, double peso, String colorPuerta) {
+        super(nombre, estatura, peso);
+        this.colorPuerta = colorPuerta;
+    }
+
+    //reir
+    //llorar
+    public void reir(){
+        System.out.println("HAHAHAHA");
+    }
+    public void llorar(){
+        System.out.println("cuñaaaaaaa, cuñaaaaaa");
+
+    }
 }
